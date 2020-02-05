06e90424e2c55c2c90115a050e11296f9a4b8eba
Greg Hilmes <99hilmes.g@gmail.com>
2020-02-05 14:12:02 -0600

    Log event dispatches.

--- plugin_bot.py
+++ plugin_bot.py
@@ -1,6 +1,7 @@
 """Plugin Bot - a Discord client designed to be used plug-n-play style.
 """
 import functools
+import logging
 
 import discord
 
@@ -11,6 +12,7 @@ class PluginBot(discord.Client):
         """Create a bot with the plugins passed."""
         super(PluginBot, self).__init__()
         self.plugins = plugins
+        self.logger = logging.getLogger(self.__class__.__name__)
 
     def dispatch(self, event, *args, **kwargs):
         super(PluginBot, self).dispatch(event, *args, **kwargs)
@@ -19,5 +21,7 @@ class PluginBot(discord.Client):
 
         for plugin in self.plugins:
             if hasattr(plugin, method):
+                self.logger.debug('dispatching %s to %s', method,
+                                  plugin.__name__)
                 coro = functools.partial(getattr(plugin, method), self)
                 self._schedule_event(coro, method, *args, **kwargs)
