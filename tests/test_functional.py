196766c67759f64181d20a5fe22e5b718ec16a71
Greg Hilmes <99hilmes.g@gmail.com>
2020-02-05 13:15:47 -0600

    Add test for calling event method of a plugin.

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,17 +1,40 @@
 """Functional tests for expected features.
 """
-
+import asyncio
+import os
 import unittest
 
 import plugin_bot
 
+TEST_TOKEN = os.environ.get('DISCORD_TEST_TOKEN')
 
-class TestCanCreateBot(unittest.TestCase):
+
+class TestCanCreateBot(unittest.IsolatedAsyncioTestCase):
     """Tests for the most basic use cases."""
     def test_create_with_no_plugins(self):
         """Test that an empty bot can be created."""
         bot = plugin_bot.PluginBot([])
         self.assertEqual(bot.plugins, list())
+
+    async def test_create_with_mock_plugin(self):
+        """Test that a basic plugin is used."""
+        class TestPlugin:
+            ready_calls = 0
+
+            async def on_ready(self):
+                TestPlugin.ready_calls += 1
+
+        bot = plugin_bot.PluginBot([TestPlugin])
+        self.assertIn(TestPlugin, bot.plugins)
+
+        # Run the bot
+        await bot.login(TEST_TOKEN)
+        try:
+            await asyncio.wait_for(bot.connect(), timeout=5)
+        except asyncio.TimeoutError:
+            await bot.close()
+
+        self.assertNotEqual(TestPlugin.ready_calls, 0, 'on_ready not called')
 
 
 if __name__ == '__main__':
